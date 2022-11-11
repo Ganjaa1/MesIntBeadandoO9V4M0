f8b0051df17e52d1e323d6d86ccd057b96843a51
EMEA\JUM1MC <fixed-term.marcelltibor.juhasz@hu.bosch.com>
2022-11-11 09:31:17 +0100

    .

--- src/app/components/solver-components/solver-components.component.ts
+++ src/app/components/solver-components/solver-components.component.ts
@@ -15,8 +15,9 @@ export class SolverComponentsComponent implements OnInit {
 
   ngOnInit(): void {
     this.generateCitiesCoordinates(10);
-    console.log(this.cities)
+    console.log('cities:',this.cities)
     this.shuffleCitiesForRiders(4);
+    this.transformTSP(this.cities)
   } 
 
   generateCitiesCoordinates(clientNumber:number){
@@ -29,19 +30,41 @@ export class SolverComponentsComponent implements OnInit {
 
   shuffleCitiesForRiders(riderNumber:number){
     let usedCities:number[] = [];
-    let tmpRiders:Rider[] = [];
+    let tmpRiders:{ routeNumber:number, city:City }[] = [];
+
     for (let index = 0; index < this.cities.length; index++) {
       let routeNumber = Math.floor(Math.random() * ((riderNumber-1) - 0) + 0);
       if(!usedCities.includes(index)){
-        tmpRiders.push({'routeNumber':routeNumber,'destinations':[this.cities[index]]})
+        tmpRiders.push({'routeNumber':routeNumber,'city':this.cities[index]})
         usedCities.push(index);
       }
     }
+
     tmpRiders = tmpRiders.reduce((prevVal:any,nextVal:any) => ({
       ...prevVal,
-      [nextVal.routeNumber]: [...(prevVal[nextVal.routeNumber] || []),[...nextVal.destinations]],
+      [nextVal.routeNumber]: [...(prevVal[nextVal.routeNumber] || []),nextVal.city],
     }),{});
-    console.log(tmpRiders)
+
+    this.riders = Object.entries(tmpRiders)
+    .map(groupedRoutes => ({'routeNumber':+groupedRoutes[0],'destinations':[...[groupedRoutes[1]]][0] as unknown as City[]}));
+    console.log(this.riders)
+  }
+
+  
+  transformTSP(tsp:City[]){
+    let tspDict:number[][] = [];
+    console.log(tspDict)
+    for (let i = 0; i < tsp.length; i++) {
+      tspDict[i] =[];
+      for (let j = 0; j < tsp.length; j++) {
+        tspDict[i][j] = this.getDistance(tsp[i], tsp[j]);
+      }
+    }
+    return tspDict;
+  }
+  
+  getDistance(city1:City,city2:City){
+    return Math.pow((Math.pow((city2.x - city1.x),2) + Math.pow((city1.y - city2.y),2)),0.5);
   }
 
 }
