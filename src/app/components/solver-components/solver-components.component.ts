0cb5303d43f9c1715e5a0301a97aa7b512959461
JMarcell <79013263+Ganjaa1@users.noreply.github.com>
2022-11-12 22:03:19 +0100

    .

--- src/app/components/solver-components/solver-components.component.ts
+++ src/app/components/solver-components/solver-components.component.ts
@@ -9,61 +9,62 @@ import { Rider } from 'src/app/models/Rider';
 })
 export class SolverComponentsComponent implements OnInit {
 
-  cities:City[] = [];
-  riders:Rider[] = [];
+  cities: City[] = [];
+  riders: Rider[] = [];
   constructor() { }
 
   ngOnInit(): void {
     this.generateCitiesCoordinates(10);
-    console.log('cities:',this.cities)
-    this.shuffleCitiesForRiders(4);
-    // for (let index = 0; index < dataIndexes.length; index++) {
-    //   console.log(`Vehicle[${index}] route: ${[...dataIndexes[index]]} \n and it's cost: ${this.objectFunction(this.transformTSP(this.cities),dataIndexes[index])}`);
-    // }
-  } 
+    console.log('cities:', this.cities)
+    this.shuffleCitiesForRiders(3);
+    console.log('riders', this.riders)
 
-  generateCitiesCoordinates(clientNumber:number){
+    let riderPaths = this.riders.map(item => item.destinations.map(dest => dest));
+    riderPaths.forEach(routes => {
+      console.log('ezvót: ', routes)
+      console.log('ez lett: ', this.localSearch(1000000, routes));
+
+    })
+  }
+
+  generateCitiesCoordinates(clientNumber: number) {
     for (let index = 0; index < clientNumber; index++) {
       let x = Math.floor(Math.random() * 10);
       let y = Math.floor(Math.random() * 10);
-      this.cities.push({x,y})
+      this.cities.push({ x, y })
     }
   }
 
-  shuffleCitiesForRiders(riderNumber:number){
-    let usedCities:number[] = [];
-    let tmpRiders:{ routeNumber:number, cityIndex:number }[] = [];
+  shuffleCitiesForRiders(riderNumber: number) {
+    let usedCities: number[] = [];
+    let tmpRiders: { routeNumber: number, cityIndex: number }[] = [];
 
-    for (let index = 0; index < this.cities.length; index++) {
-      let routeNumber = Math.floor(Math.random() * ((riderNumber-1) - 0) + 0);
-      if(!usedCities.includes(index)){
-        tmpRiders.push({'routeNumber':routeNumber,'cityIndex':index})
+    this.cities.forEach((_city, index) => {
+      let routeNumber = Math.floor(Math.random() * ((riderNumber) - 0) + 0);
+      if (!usedCities.includes(index)) {
+        tmpRiders.push({ 'routeNumber': routeNumber, 'cityIndex': index })
         usedCities.push(index);
       }
-    }
+    })
 
-    tmpRiders = tmpRiders.reduce((prevVal:any,nextVal:any) => ({
+    tmpRiders = tmpRiders.reduce((prevVal: any, nextVal: any) => ({
       ...prevVal,
-      [nextVal.routeNumber]: [...(prevVal[nextVal.routeNumber] || []),nextVal.cityIndex],
-    }),{});
+      [nextVal.routeNumber]: [...(prevVal[nextVal.routeNumber] || []), nextVal.cityIndex],
+    }), {});
 
-    this.riders = Object.entries(tmpRiders)
-    .map(groupedRoutes => ({'routeNumber':+groupedRoutes[0],'destinations':[...[groupedRoutes[1]]][0] as unknown as number[]}));
-    console.log('riders',this.riders)
+    this.riders = Object.entries(tmpRiders).map(groupedRoutes => ({ 'routeNumber': +groupedRoutes[0], 'destinations': [...[groupedRoutes[1]]][0] as unknown as number[] }));
   }
 
-  transformTSP(tsp:City[]){
-    let tspDict:number[][] = [];
-    for (let i = 0; i < tsp.length; i++) {
-      tspDict[i] =[];
-      for (let j = 0; j < tsp.length; j++) {
-        tspDict[i][j] = this.getDistance(tsp[i], tsp[j]);
-      }
-    }
+  transformTSP(tsp: City[]) {
+    let tspDict: number[][] = [];
+    tsp.forEach((_city, i) => {
+      tspDict[i] = [];
+      tsp.forEach((_city, j) => tspDict[i][j] = this.getDistance(tsp[i], tsp[j]));
+    })
     return tspDict;
   }
 
-  objectFunction(dictionary:number[][],s:any){
+  objectFunction(dictionary: number[][], s: any) {
     let dist = 0;
     let prev = s[0];
     for (let i = 0; i < s.length; i++) {
@@ -75,22 +76,94 @@ export class SolverComponentsComponent implements OnInit {
 
   }
 
-  localSearch(iterations:number){
-    let tsp_dict = this.transformTSP(this.cities);
-    let riderPaths = this.riders.map(item => item.destinations.map(dest => dest));
-    for (let i = 0; i < riderPaths.length; i++) {
-      //itt úgy vizsgálódni, hogy ez a loop nézi hogy az elsőhöz rendelve hogy a legjobb, majd megy a kövi sorra, amiken már átment elmenti
+  localSearch(iterations: number, routes: number[]) {
+    let tmpRoutes = [...routes];
+    let tspDictionary = this.transformTSP(this.cities);
+    let bestSearch = this.objectFunction(tspDictionary, tmpRoutes);
+    let bestRoutes = tmpRoutes;
+
+    for (let i = 0; i < iterations; i++) {
+      tmpRoutes = [...routes]
+      let a: number = Math.floor(Math.random() * ((tmpRoutes.length - 1) - 0) + 0);
+      let b: number = Math.floor(Math.random() * ((tmpRoutes.length - 1) - 0) + 0);
+      let tmp = tmpRoutes[a];
+      tmpRoutes[a] = tmpRoutes[b];
+      tmpRoutes[b] = tmp;
+      let currenRoutesSearchCalc = this.objectFunction(tspDictionary, tmpRoutes);
+
+      if (currenRoutesSearchCalc < bestSearch && a != b) {
+        bestSearch = currenRoutesSearchCalc;
+        bestRoutes = tmpRoutes;
+      }
+
     }
-    // for (let index = 0; index < dataIndexes.length; index++) {
-    //   console.log(`Vehicle[${index}] route: ${[...dataIndexes[index]]} \n and it's cost: ${this.objectFunction(this.transformTSP(this.cities),dataIndexes[index])}`);
-    // }
-    // let objectF = this.objectFunction(tsp_dict,s)
-    // let sBest = s;
-    //let fBest =
+    return bestRoutes;
   }
-  
-  getDistance(city1:City,city2:City){
-    return Math.pow((Math.pow((city2.x - city1.x),2) + Math.pow((city1.y - city2.y),2)),0.5);
+
+  getBestBetweenRiders(iterations: number) {
+    let riderPaths = [...this.riders.map(item => item.destinations.map(dest => dest))];
+    riderPaths.forEach(route => {
+      let bestRowRoutes = this.localSearch(1000000, route);
+      bestRowRoutes.forEach(bestRow => {
+
+      })
+      // route.forEach(routeItem => {
+
+      // })
+    })
+    // for (let i = 0; i < riderPaths.length; i++) {
+    //   let bestRowRoutes = this.localSearch(1000000, riderPaths[i]);
+    //   for (let j = 0; j < riderPaths[i].length; j++) {
+
+    //   }
+
+    // }
+  }
+
+  // localSearch(iterations: number) {
+  //   let usedCities: number[] = [];
+  //   let tsp_dict = this.transformTSP(this.cities);
+  //   let riderPaths = this.riders.map(item => item.destinations.map(dest => dest));
+  //   for (let i = 0; i < riderPaths.length; i++) {
+  //     let currentBest = this.objectFunction(tsp_dict, riderPaths[i]);
+  //     let currentBestRoutes: number[] = riderPaths[i];
+  //     console.log("startedBest: ", currentBest, 'indexes: ', currentBestRoutes)
+  //     for (let j = 0; j < iterations; j++) {
+  //       let randomLength: number = Math.floor(Math.random() * ((this.cities.length - 1) - currentBestRoutes.length) + currentBestRoutes.length)
+  //       let currentPathShuffledRoutes: number[];
+  //       randomLength = Math.floor(Math.random() * ((this.cities.length - 1) - currentBestRoutes.length) + currentBestRoutes.length);
+  //       currentPathShuffledRoutes =
+  //         [...new Set(
+  //           [...new Array(randomLength)].map(() =>
+  //             Math.floor(Math.random() * (this.cities.length - 1 - 0) + 0)
+  //           )
+  //         ),
+  //         ];
+  //       let currentPathCalc = this.objectFunction(tsp_dict, currentPathShuffledRoutes);
+
+  //       if (currentPathCalc < currentBest && currentPathCalc
+  //         && currentPathShuffledRoutes.length != 0 && !usedCities.some(city => currentPathShuffledRoutes.includes(city))) {
+  //         currentBestRoutes = currentPathShuffledRoutes;
+  //         currentBest = currentPathCalc;
+  //       }
+  //     }
+  //     currentBestRoutes.forEach(route => usedCities.push(route))
+  //     console.log("finishedBest: ", currentBest, 'indexes: ', currentBestRoutes, '\n')
+  //     console.log("fasz")
+
+  //   }
+  //   console.log(usedCities)
+  // }
+  //itt úgy vizsgálódni, hogy ez a loop nézi hogy az elsőhöz rendelve hogy a legjobb, majd megy a kövi sorra, amiken már átment elmenti
+  // for (let index = 0; index < dataIndexes.length; index++) {
+  //   console.log(`Vehicle[${index}] route:   {[...dataIndexes[index]]} \n and it's cost: ${this.objectFunction(this.transformTSP(this.cities),dataIndexes[index])}`);
+  // } 
+  // let objectF = this.objectFunction(tsp_dict,s)
+  // let sBest = s;
+  //let fBest =
+
+  getDistance(city1: City, city2: City) {
+    return Math.pow((Math.pow((city2.x - city1.x), 2) + Math.pow((city1.y - city2.y), 2)), 0.5);
   }
 
 }
