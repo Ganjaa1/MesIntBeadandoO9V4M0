8a9a910ea15133acbb19f722763d455c6075f39b
EMEA\JUM1MC <fixed-term.marcelltibor.juhasz@hu.bosch.com>
2022-11-14 14:07:43 +0100

    half working

--- src/app/components/solver-components/solver-components.component.ts
+++ src/app/components/solver-components/solver-components.component.ts
@@ -11,21 +11,16 @@ export class SolverComponentsComponent implements OnInit {
 
   cities: City[] = [];
   riders: Rider[] = [];
+  tspDictionary: any[] = [];
   constructor() { }
 
   ngOnInit(): void {
     this.generateCitiesCoordinates(10);
+    this.tspDictionary = this.transformTSP(this.cities);
     console.log('cities:', this.cities)
     this.shuffleCitiesForRiders(3);
     console.log('riders', this.riders)
-
-    let riderPaths = this.riders.map(item => item.destinations.map(dest => dest));
-    riderPaths.forEach(routes => {
-      console.log('ezvót: ', routes)
-      console.log('ez lett: ', this.localSearch(1000000, routes));
-
-    })
-    this.getBestBetweenRiders(10)
+    this.getBestBetweenRiders(10000)
   }
 
   generateCitiesCoordinates(clientNumber: number) {
@@ -65,32 +60,31 @@ export class SolverComponentsComponent implements OnInit {
     return tspDict;
   }
 
-  objectFunction(dictionary: number[][], s: any) {
+  objectFunction(s: any[]) {
     let dist = 0;
     let prev = s[0];
     for (let i = 0; i < s.length; i++) {
-      dist += dictionary[prev][i];
+      dist += this.tspDictionary[prev][i];
       prev = i;
     }
-    dist += dictionary[s.slice(-1)[0]][0];
+    dist += this.tspDictionary[s.slice(-1)[0]][0];
     return dist;
 
   }
 
   localSearch(iterations: number, routes: number[]) {
     let tmpRoutes = [...routes];
-    let tspDictionary = this.transformTSP(this.cities);
-    let bestSearch = this.objectFunction(tspDictionary, tmpRoutes);
+    let bestSearch = this.objectFunction(tmpRoutes);
     let bestRoutes = tmpRoutes;
 
     for (let i = 0; i < iterations; i++) {
       tmpRoutes = [...routes]
-      let a: number = Math.floor(Math.random() * ((tmpRoutes.length - 1) - 0) + 0);
-      let b: number = Math.floor(Math.random() * ((tmpRoutes.length - 1) - 0) + 0);
+      let a: number = Math.floor(Math.random() * ((tmpRoutes.length - 1)));
+      let b: number = Math.floor(Math.random() * ((tmpRoutes.length - 1)));
       let tmp = tmpRoutes[a];
       tmpRoutes[a] = tmpRoutes[b];
       tmpRoutes[b] = tmp;
-      let currenRoutesSearchCalc = this.objectFunction(tspDictionary, tmpRoutes);
+      let currenRoutesSearchCalc = this.objectFunction(tmpRoutes);
 
       if (currenRoutesSearchCalc < bestSearch && a != b) {
         bestSearch = currenRoutesSearchCalc;
@@ -102,30 +96,74 @@ export class SolverComponentsComponent implements OnInit {
   }
 
   getBestBetweenRiders(iterations: number) {
-    let riderPaths = [...this.riders.map(item => item.destinations.map(dest => dest))];
-    riderPaths.forEach(route => {
-      let bestRowRoutes = this.localSearch(1000000, route);
-      bestRowRoutes.forEach(bestRow => {
-        let rowIndex: number = Math.floor(Math.random() * ((this.riders.length - 1) - 0) + 0);
-        let columnIndex: number = Math.floor(Math.random() * ((route.length - 1) - 0) + 0);
-        console.log(rowIndex, ' ', columnIndex, ' ', this.riders[rowIndex].destinations[columnIndex])
-      })
-      //amit kivesz azt cserélgetni, hogy lenne jobb mint volt, felülírni, de úgy hogy azt még később tudjam használni
-      // route.forEach(routeItem => {
-
-      // })
-    })
-    // for (let i = 0; i < riderPaths.length; i++) {
-    //   let bestRowRoutes = this.localSearch(1000000, riderPaths[i]);
-    //   for (let j = 0; j < riderPaths[i].length; j++) {
-
-    //   }
-
-    // }
+    let riderRoutes = [...this.riders.map(item => [...item.destinations])];
+    let optimalRoutes: number[][] = [...riderRoutes];
+    let usedCities: number[] = [];
   }
+
+  // getBestBetweenRiders(iterations: number) {
+  //   let riderRoutes = [...this.riders.map(item => [...item.destinations])];
+  //   let optimalRoutes: number[][] = [...riderRoutes];
+  //   let usedCities: number[] = [];
+
+  //   riderRoutes.forEach((route,index) => {
+  //     optimalRoutes[index] = [];
+  //     let localBestRoutes = [...this.localSearch(1000000, route)];
+  //     let localBestCalc = this.objectFunction(localBestRoutes);
+
+  //     for (let i = 0; i < iterations; i++) {
+  //       let rowIndex: number = Math.floor(Math.random() * ((riderRoutes.length - 1)));
+  //       let columnIndex: number = Math.floor(Math.random() * ((riderRoutes[rowIndex].length - 1)));
+  //       let currentRowIndex: number = Math.floor(Math.random() * ((route.length - 1)));
+
+  //       if(!usedCities.includes(riderRoutes[rowIndex][columnIndex])){
+  //         let currentRoutes = [...localBestRoutes];
+  //         let tmp = currentRoutes[currentRowIndex];
+  //         currentRoutes[currentRowIndex] = riderRoutes[rowIndex][columnIndex];
+  //         let currentCalc = this.objectFunction(currentRoutes);
+
+  //         if(currentCalc < localBestCalc && !usedCities.includes(riderRoutes[rowIndex][columnIndex])){
+  //           console.log('ez jobb: ',currentCalc, ' mint: ',localBestCalc,' ezzel:', currentRoutes, ' ehelyett: ',localBestRoutes);
+  //           localBestCalc = currentCalc;
+  //           localBestRoutes = currentRoutes;
+  //           currentRoutes[currentRowIndex] = riderRoutes[rowIndex][columnIndex];
+  //           riderRoutes[rowIndex][columnIndex] = tmp;
+  //         }
+  //       }
+  //     }
+  //     localBestRoutes.forEach(route => usedCities.push(route))
+  //     optimalRoutes[index]= localBestRoutes;
+  //   })
+  //   console.log('vége')
+  //   optimalRoutes.forEach((route,index) => {
+  //     console.log(`rider[${index}]: \n route:${route} \n cost: ${this.objectFunction(route)}`)
+  //   })
+  //   this.cities.forEach((city,index) => {
+  //     console.table(`(${city.x},${city.y})\n`)
+  //   })
+  //   //console.table(this.cities)
+  // }
 
   getDistance(city1: City, city2: City) {
     return Math.pow((Math.pow((city2.x - city1.x), 2) + Math.pow((city1.y - city2.y), 2)), 0.5);
   }
 
 }
+      // let tmpCurrentArray = [...localBestRoutes];
+      // localBestRoutes.forEach((_currentItem,currentItemIndex) => {
+      //   riderRoutes.filter(rider => rider != route).forEach(notCurrentArray =>{
+      //     //console.log(tmpCurrentArray, notCurrentArray)
+      //       notCurrentArray.forEach(notCurrentItem =>{
+      //         tmpCurrentArray[currentItemIndex] = notCurrentItem;
+      //         let currentRoutes= this.localSearch(1000000,tmpCurrentArray);
+      //         let currentCalc = this.objectFunction(currentRoutes);
+      //         //console.log(currentCalc, currentRoutes)
+      //         if(currentCalc < localBestCalc && !usedCities.some(city => notCurrentArray.indexOf(city) >=0)){
+      //           console.log('ez jobb: ',currentCalc, ' mint: ',localBestCalc,' ezzel:', currentRoutes, ' ehelyett: ',localBestRoutes);
+      //           localBestCalc = currentCalc;
+      //           localBestRoutes = currentRoutes;
+      //           localBestRoutes.forEach(route => usedCities.push(route))
+      //           optimalRoutes[index] = [...new Set(currentRoutes)]
+      //         }
+      //       })
+      //   })
